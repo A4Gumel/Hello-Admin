0c7df583a3027f919815edccc49c2d29905c3539
auwal <a4auwal@gmail.com>
2022-03-08 22:13:14 +0100

    Profile detail and edit page complete

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,5 +1,5 @@
 import './user.css'
-import {PermIdentityRounded, MailOutlineRounded, LocationCityRounded, PhoneRounded, CalendarTodayOutlined} from "@mui/icons-material";
+import {PermIdentityRounded, MailOutlineRounded, LocationCityRounded, PhoneRounded, CalendarTodayOutlined, PublishRounded} from "@mui/icons-material";
 
 export default function User() {
   return (
@@ -45,7 +45,53 @@ export default function User() {
             </div>
           </div>
         </div>
-        <div className="userUpdate"></div>
+        <div className="userUpdate">
+          <span className="userUpdateTitle">Edit</span>
+          <form action="" className="userUpdateForm">
+            <div className="userUpdateLeft">
+              <div className="userUpdateItem">
+                <label htmlFor="">Username</label>
+                <input input="text" 
+                placeholder='engrsilver' 
+                className='userUpdateInput'/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="">Full Name</label>
+                <input input="text" 
+                placeholder='Silver Angry Bird' 
+                className='userUpdateInput'/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="">Email</label>
+                <input input="text" 
+                placeholder='engr@angrybird.com' 
+                className='userUpdateInput'/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="">Phone</label>
+                <input input="text" 
+                placeholder='+123 7985 62662' 
+                className='userUpdateInput'/>
+              </div>
+              <div className="userUpdateItem">
+                <label htmlFor="">Location</label>
+                <input input="text" 
+                placeholder='Kano | Nigeria.' 
+                className='userUpdateInput'/>
+              </div>
+            </div>
+            <div className="userUpdateRight">
+              <div className="userUpdateUpload">
+                <img src="https://vignette.wikia.nocookie.net/theangrybirdsmovie2/images/0/08/Silver_Angry_Birds_(1)_-_Edited.jpg/revision/latest?cb=20190505004816" 
+                alt="" 
+                className="userUpdateImg" />
+                <label htmlFor="file"><PublishRounded className="userUpdateIcon"/></label>
+                <input type="file" id="file" style={{ display: "none" }}></input>
+              </div>
+              <button className="userUpdateButton">Update</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
